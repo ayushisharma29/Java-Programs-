926b58b5456c0d4ade1de5af7e5f34814cc442f0
Ayushi Sharma <145185573+ayushisharma29@users.noreply.github.com>
2025-07-12 23:54:45 +0530

    new file

---
+++ ShoppingCartMain.java
@@ -0,0 +1,42 @@
+class QuantityException extends Exception
+{
+	public String getMessage()
+	{
+		return "Item is invalid!";
+	}
+}
+class ShoppingCart
+{
+	String name;
+	int quantity;
+	void addItem(String name, int quantity)throws QuantityException
+	{
+		this.name = name;
+		this.quantity = quantity;
+		if(quantity<=0)
+		{
+			throw  new QuantityException();
+		}
+	}
+}
+class ShoppingCartMain
+{
+	public static void main(String args[]) 
+	{
+		String name = args[0];
+		int quantity = Integer.parseInt(args[1]);
+		ShoppingCart item[] = new ShoppingCart(name, quantity);
+		try
+		{
+			item.addItem(name,quantity);
+			System.out.println(item);
+		}
+		catch(Exception qe)
+		{	
+			System.out.println(qe.getMessage());
+		}
+	}
+}
+			
+	
+	
